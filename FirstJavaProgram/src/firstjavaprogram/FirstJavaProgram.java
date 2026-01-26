4ea35aeb8b5d6a43e0fc89cb9d6c9298b3ee071d
Md. Abul Hasan <abulhasan0292@gmail.com>
2026-01-26 18:58:58 +0600

    add

---
+++ FirstJavaProgram/src/firstjavaprogram/FirstJavaProgram.java
@@ -0,0 +1,16 @@
+
+    
+  
+    public static void main(String[] args) {
+     
+    float radious;
+       float area;
+       radius = 5 ;
+       area =(float)*radious*radious;
+       system.out.printitIn("Area of a circle is "+area)");
+    }
+   
+
+
+
+
